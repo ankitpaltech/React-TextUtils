cd6bb3dd9096c5ef2e6736c0bf71ca7353659718
unknown <ankitpal4tech@gmail.com>
2023-03-17 12:48:40 +0530

    added LowerCase and Clear Button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,35 +1,73 @@
 import React , {useState} from 'react'
-import Button from 'react-bootstrap/Button';
+
 import Form from 'react-bootstrap/Form';
 
 export default function TextForm(props) {
-    const handleUpClick =() => {
-        // console.log("changetoUpperchase" + text);
-        let newText = text.toUpperCase();
-        setText( newText)
-    };
-    const handleOnChange = (event) =>{
-        // console.log("change on click ");
-        setText(event.target.value);
 
-    };
-    const[text , setText ] = useState("Enter the text ");
+
+        const handleUpClick =() => {
+          // console.log("changetoUpperchase" + text);
+          let newText = text.toUpperCase();
+          setText( newText);
+        };
+        const handleLoClick =() => {
+          // console.log("changetoUpperchase" + text);
+          let newText = text.toLowerCase();
+          setText( newText);
+        };
+
+        const handleOnChange = (event) =>{
+          // console.log("change on click ");
+          setText(event.target.value);
+
+        };
+        const handlecleartext = (event) =>{
+        
+          let newText = " ";
+          setText( newText);
+        };
+        
+ 
+    const[text , setText ] = useState("");
   return (
     <>
-    <Form>
-      <fieldset>
+    
+      
+       
+        <div className=" container mb-3">
+
         <h1>{props.heading}</h1>
-        <Form.Group className="mb-3">
-          
-        <Form.Control id="myBox" value= {text} onChange = {handleOnChange} rows = "8"/>
-        </Form.Group>
+        <textarea name="" id="myBox" cols="120" rows="8" value= {text} onChange = {handleOnChange}></textarea>
+        <br />
         <button className="btn btn-primary mx-3" onClick={handleUpClick}>
             Convert To UpperCase
         </button>
+        <button className="btn btn-primary mx-3" onClick={handleLoClick}>
+            Convert To LowerCase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handlecleartext}>
+            text cleared 
+        </button>
 
-        <Button type="submit">Submit</Button>
-      </fieldset>
-    </Form>
+        </div>
+
+        <div className="container my-3">
+        <h1>Your Text Summery</h1>
+        <p>{text.split(" ").length} words and {text.length} characters.</p>
+        <p>{0.008*text.split(" ").length} Mintues to read.</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+        </div>
+        
+
+       
+     
+     
+        
+
+       
+      
+    
     </>
   )
 }
